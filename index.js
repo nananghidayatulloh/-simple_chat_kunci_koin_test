89c7e859162de23dd5655fdf8a571ed6265b359a
nananghidayatulloh <nananghidayatulloh@gmail.com>
2022-04-25 13:55:40 +0700

    install express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const path = require('path')
+
+app.use(
+  express.static( path.join(__dirname, '/static') )
+)
+
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hallo Bos</h1>')
+})
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+    console.log('listening on: ', port)
+})
